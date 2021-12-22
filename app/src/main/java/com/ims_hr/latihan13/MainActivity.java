e20096ec1d2ebb3814be8487bff2d771b0b081e8
Irsyad Purbha <irsyadpurbha@gmail.com>
2021-12-22 21:32:44 +0700

    UPDATE 20211222 21:07

--- app/src/main/java/com/ims_hr/latihan13/MainActivity.java
+++ app/src/main/java/com/ims_hr/latihan13/MainActivity.java
@@ -2,11 +2,12 @@ package com.ims_hr.latihan13;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
